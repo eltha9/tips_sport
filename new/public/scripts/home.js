9019e6e1f3fc82787d23db5c533edb14691ba18c
eltha9 <theadosto@gmail.com>
2020-04-10 21:52:47 +0200

    wip home

--- new/public/scripts/home.js
+++ new/public/scripts/home.js
@@ -1,11 +1,14 @@
 const home_main = document.querySelector('main')
+const logo = document.querySelector('h1')
 const go_to_connect = document.querySelector('.to-connect button')
 const go_to_create = document.querySelector('.to-create button')
 
 go_to_connect.addEventListener('click', ()=>{
     home_main.classList.toggle('connect')
+    logo.classList.toggle('blue')
 })
 
 go_to_create.addEventListener('click', ()=>{
     home_main.classList.toggle('connect')
+    logo.classList.toggle('blue')
 })
