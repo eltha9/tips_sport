ee39a3ee3659c41cd9a9e3124ad725ed0b91173a
eltha9 <theadosto@gmail.com>
2020-04-12 19:20:01 +0200

    add rooting, start dasboard

--- new/public/index.php
+++ new/public/index.php
@@ -1,4 +1,27 @@
 <?php
 
 
-require('../components/home.html');
+// require('../components/home.html');
+
+
+if( isset($_GET['page'])){
+    $page = $_GET['page'];
+
+}else{
+    require('../components/home.html');
+
+    die;
+}
+
+switch($page){
+    case '':
+    case 'home':
+        require('../components/home.html');
+    break;
+    case 'dashboard':
+        require('../components/dashboard.html');
+    break;
+    default:
+        require('../components/home.html');
+}
+
